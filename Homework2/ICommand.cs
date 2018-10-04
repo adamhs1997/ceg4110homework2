1da291c244a7b60064bd8120cb7f02c5b9453a9f
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-10-03 21:37:36 -0400

    Add command interface

---
+++ Homework2/ICommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework2 {
+
+    // Interface for all generic command implementations
+
+    interface ICommand {
+
+        // "You can only do and undo a command..."
+        void Execute();
+        void Revert();
+
+    }
+
+}
