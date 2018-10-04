4b3f1861d8e3ae87e778bf1e4b9ce2ba9490ec84
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-10-03 22:38:31 -0400

    Convert model to directly modifying current DT object

--- Homework2/ClockModel.cs
+++ Homework2/ClockModel.cs
@@ -12,49 +12,57 @@ namespace Homework2 {
         private static ClockModel _model = new ClockModel();
         DateTime _current = DateTime.Now;
         private ClockController _controller;
-        private int _second;
-        private int _minute;
-        private int _hour;
-        private int _day;
-        private string _month;
-        private int _year;
 
         // Private constructor implementing Singleton DP
         private ClockModel() {
-            // Initialize to current times
-            _second = _current.Second;
-            _minute = _current.Minute;
-            _hour = _current.Hour;
-            _day = _current.Day;
-            _month = _current.ToString("MMMM");
-            _year = _current.Year;
-
             // Begin ticking the clock
             AdvanceClock();
         }
 
         // Public-facing getters and setters
-        // If you aren't familiar with this C# syntax, read more about this here:
-        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/auto-implemented-properties
         public static ClockModel Model { get => _model; }
-        public int Second { get => _second; set => _second = value; }
-        public int Minute { get => _minute; set => _minute = value; }
-        public int Hour { get => _hour; set => _hour = value; }
-        public int Day { get => _day; set => _day = value; }
-        public string Month { get => _month; set => _month = value; }
-        public int Year { get => _year; set => _year = value; }
+
+        public int Second {
+            get => _current.Second;
+            set => _current = new DateTime(_current.Year, _current.Month,
+                _current.Day, _current.Hour, _current.Minute, value);
+        }
+
+        public int Minute {
+            get => _current.Minute;
+            set => _current = new DateTime(_current.Year, _current.Month,
+                _current.Day, _current.Hour, value, _current.Second);
+        }
+
+        public int Hour {
+            get => _current.Hour;
+            set => _current = new DateTime(_current.Year, _current.Month,
+                _current.Day, value, _current.Minute, _current.Second);
+        }
+
+        public int Day {
+            get => _current.Day;
+            set => _current = new DateTime(_current.Year, _current.Month,
+                value, _current.Hour, _current.Minute, _current.Second);
+        }
+
+        public int Month {
+            get => _current.Month;
+            set => _current = new DateTime(_current.Year, value,
+                _current.Day, _current.Hour, _current.Minute, _current.Second);
+        }
+
+        public int Year {
+            get => _current.Year;
+            set => _current = new DateTime(value, _current.Month,
+                _current.Day, _current.Hour, _current.Minute, _current.Second);
+        }
 
         // Asynchronous method call to update views on the second
         // Courtesy: https://forums.xamarin.com/discussion/37393/how-to-update-a-viewtext-every-0-1-sec
-        public async Task AdvanceClock() {
+        public async void AdvanceClock() {
             while (true) {
                 _current = _current.AddSeconds(1);
-                _second = _current.Second;
-                _minute = _current.Minute;
-                _hour = _current.Hour;
-                _day = _current.Day;
-                _month = _current.ToString("MMMM");
-                _year = _current.Year;
                 await Task.Delay(1000, new CancellationToken());
             }
         }
