d6aa1413595739370dd8f8694a1d9cbb461f8bbc
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-10-03 22:41:59 -0400

    Create command list singleton to store undo/redo

--- Homework2/CommandList.cs
+++ Homework2/CommandList.cs
@@ -1,16 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using System.Collections.Generic;
 
 namespace Homework2 {
-    class Class1 {
+
+    // Singleton class that will hold all our commands to undo/redo
+
+    class CommandList {
+
+        // Create our instance of the list
+        private static List<ICommand> _list = new List<ICommand>();
+
+        // Hold on to the singleton command list
+        private static CommandList _commandList = new CommandList();
+
+        // Hold on to current and max indices in list
+        private int _currentPos = 0, _maxPos = 0;
+
+        // Privatize our constructor
+        private CommandList() { }
+
+        // Make possible to get back our list
+        public static CommandList Commands { get => _commandList; }
+
+        // Add a new item to the end of the list
+        public void AddCommand(ICommand command) {
+            _list.Add(command);
+            _currentPos++;
+            _maxPos = _currentPos;
+        }
+
+        // Undo the last item in the list
+        public void Undo() {
+            _list[_currentPos--].Revert();
+        }
+
+        // Redo the next command
+        public void Redo() {
+            if (_currentPos < _maxPos)
+                _list[_currentPos++].Execute();
+        }
+
     }
+
 }
