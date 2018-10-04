148673d1ce4e5623e3606833c782c421f073accd
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-10-03 23:03:09 -0400

    Rename generic change hour to increment hour
    Support only one change in hour per command

---
+++ Homework2/IncrementHour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Homework2 {
+
+    // Concrete command to implement hour increases
+
+    class IncrementHour : ICommand {
+
+        // Hold our singleton command list and controller
+        private CommandList _commands = CommandList.Commands;
+        private ClockController _controller = ClockController.Controller;
+
+        public void Execute() {
+            if (_controller.GetHour() != 23)
+                _controller.SetHour(_controller.GetHour() + 1);
+            else
+                _controller.SetHour(0);
+        }
+
+        public void Revert() {
+            if (_controller.GetHour() != 0)
+                _controller.SetHour(_controller.GetHour() - 1);
+            else
+                _controller.SetHour(23);
+        }
+
+    }
+}
