90c6949160bcfb355fc542dc39de1c082b4fea96
Adam Horvath-Smith <horvath-smith.2@wright.edu>
2018-10-08 11:05:01 -0400

    Add date to analog clock

--- Homework2/AnalogClockView.cs
+++ Homework2/AnalogClockView.cs
@@ -9,54 +9,73 @@ using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Util;
 using Android.Widget;
 
 using Java.Util;
 
 namespace Homework2 {
-    [Activity(Label = "AnalogClockView")]
+
+    [Activity(Label = "AnalogClockView", MainLauncher = true)]
+
     public class AnalogClockView : Activity, IClockView {
 
-        CustomAnalogClock clock;
-        ClockController clockController = ClockController.Controller;
+        // Define what we will need on a class level
+        private CustomAnalogClock clock;
+        private TextView analogDate;
+        private ClockController clockController = ClockController.Controller;
+
+        // Use this to show the proper month in the view
+        string[] months = {"January", "February", "March", "April", "May",
+                            "June", "July", "August", "September", "October",
+                            "November", "December"};
 
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
 
             // Programmatically create main layout
-            LinearLayout mainLayout = new LinearLayout(this);
-            LinearLayout.LayoutParams relParentParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, RelativeLayout.LayoutParams.MatchParent);
-            mainLayout.LayoutParameters = relParentParam;
+            // -->For some reason, the clock won't appear in the XML
 
-            LinearLayout.LayoutParams clockParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WrapContent, RelativeLayout.LayoutParams.WrapContent);
+            // Define basic layouts
+            LinearLayout.LayoutParams matchParentLayout =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent);
+            LinearLayout.LayoutParams wrapContentLayout =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
 
-            // Add (temp) settings button
-            Button settings = new Button(this) {
-                LayoutParameters = clockParam,
-                Text = "Settings"
+            // Create the main layout container
+            LinearLayout mainLayout = new LinearLayout(this) {
+                LayoutParameters = matchParentLayout,
+                Orientation = Orientation.Vertical
             };
-            mainLayout.AddView(settings);
-            settings.Click += Settings_Click;
+
+            // Add the calendar to the view
+            analogDate = new TextView(this) {
+                Gravity = GravityFlags.Center,
+            };
+            analogDate.SetTextColor(Color.White);
+            analogDate.SetTextSize(ComplexUnitType.Sp, 24);
+            mainLayout.AddView(analogDate);
 
             // Add clock to layout
             clock = new CustomAnalogClock(this);
             clock.SetColor(Color.White);
-            clock.LayoutParameters = clockParam;
+            clock.LayoutParameters = wrapContentLayout;
             mainLayout.AddView(clock);
 
+            // Register this view
             clockController.RegisterView(this);
 
-            SetContentView(mainLayout, relParentParam);
+            // Set our view in the activity
+            SetContentView(mainLayout, matchParentLayout);
         }
 
-        private void Settings_Click(object sender, EventArgs e) {
-            StartActivity(typeof(SettingsView));
-        }
-
+        // Implementation of the IClockView interface, allowing updates from controller
         public void DisplayTime(int hour, int minute, int second, int month, int day, int year) {
             Calendar temp = Calendar.GetInstance(new Locale("en", "US"));
             temp.Set(year, month, day, hour, minute, second);
             clock.SetCalendar(temp);
+            analogDate.Text =
+                months[month - 1] + " " + day.ToString() + ", " + year.ToString();
         }
     }
 }
